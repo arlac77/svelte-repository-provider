f28534ce296ec62f37e50b085bb58f682d45bb30
Markus Felten <Markus.Felten@gmx.de>
2020-07-09 21:51:20 +0200

    wip

--- src/index.svelte
+++ src/index.svelte
@@ -1,2 +1,31 @@
 <script context="module">
+  import Branch from "./Branch.svelte";
+  import BranchCard from "./BranchCard.svelte";
+  import BranchLink from "./BranchLink.svelte";
+  import BranchRoutes from "./BranchRoutes.svelte";
+  import Hooks from "./Hooks.svelte";
+  import Hook from "./Hook.svelte";
+  import HookCard from "./HookCard.svelte";
+  import HookLink from "./HookLink.svelte";
+  import HookRoutes from "./HookRoutes.svelte";
+  import PullRequest from "./PullRequest.svelte";
+  import PullRequestCard from "./PullRequestCard.svelte";
+  import PullRequestLink from "./PullRequestLink.svelte";
+  import PullRequestRoutes from "./PullRequestRoutes.svelte";
+
+  export {
+    Branch,
+    BranchCard,
+    BranchLink,
+    BranchRoutes,
+    Hooks,
+    Hook,
+    HookCard,
+    HookLink,
+    HookRoutes,
+    PullRequest,
+    PullRequestCard,
+    PullRequestLink,
+    PullRequestRoutes
+  };
 </script>
