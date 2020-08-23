61b5a703c4d8b81b6d9645923fc148827452666d
Markus Felten <Markus.Felten@gmx.de>
2020-08-23 17:54:14 +0200

    feat: add PullRequestLink

--- src/index.svelte
+++ src/index.svelte
@@ -3,9 +3,19 @@
   import AttributeField from "./AttributeField.svelte";
   import SecureAttributeField from "./SecureAttributeField.svelte";
   import PullRequestCard from "./PullRequestCard.svelte";
+  import PullRequestLink from "./PullRequestLink.svelte";
   import RepositoryGroupCard from "./RepositoryGroupCard.svelte";
   import HookCard from "./HookCard.svelte";
   import HookLink from "./HookLink.svelte";
 
-  export { Attributes, AttributeField, SecureAttributeField, PullRequestCard, RepositoryGroupCard, HookCard, HookLink };
+  export {
+    Attributes,
+    AttributeField,
+    SecureAttributeField,
+    PullRequestCard,
+    PullRequestLink,
+    RepositoryGroupCard,
+    HookCard,
+    HookLink
+  };
 </script>
