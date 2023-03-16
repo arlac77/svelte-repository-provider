c61c2b224e4564b07e3981375059aedbf8ae08c1
Markus Felten <Markus.Felten@gmx.de>
2023-03-17 00:23:31 +0100

    fix: cleanup

--- tests/cafe/attribute.mjs
+++ tests/cafe/attribute.mjs
@@ -1,12 +1,10 @@
 import { Selector } from "testcafe";
 
-export const base =
-  "http://localhost:5173/";
+export const base = "http://localhost:5173/";
 
 fixture`attributes`.page`${base}`;
 
 test("attributes", async t => {
-  await t.expect(Selector('#a1').value).contains('value a1');
-  await t.expect(Selector('#s1').value).contains('value s1');
+  await t.expect(Selector("#a1").value).contains("value a1");
+  await t.expect(Selector("#s1").value).contains("value s1");
 });
-
