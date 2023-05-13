887f3b1461b5b4ba05c3d03b72957aad8ddc2a64
Markus Felten <Markus.Felten@gmx.de>
2023-05-13 20:09:10 +0200

    fix: nav in example

--- tests/app/src/App.svelte
+++ tests/app/src/App.svelte
@@ -1,35 +1,15 @@
 <script>
-  import { Router } from "svelte-guard-history-router";
-  import { MultiGroupProvider, PullRequest, Hook } from "repository-provider";
-  import { StringContentEntry } from "content-entry";
-  import {
-    AttributeField,
-    SecureAttributeField,
-    PullRequestCard,
-    RepositoryGroupCard,
-    HookCard,
-    ContentEntryCard
-  } from "../../../src/index.svelte";
-
-  const provider = new MultiGroupProvider();
-  const repositoryGroup = provider.addRepositoryGroup("rg1");
-  const repository = repositoryGroup.addRepository("r1");
-  const b1 = repository.addBranch("b1");
-  const b2 = repository.addBranch("b2");
-  const pullRequest = new PullRequest(b1, b2, 4711);
-
-  const hook = new Hook(repository, "h1");
-
-  const entry = new StringContentEntry("entry name", "lines 1");
+  import { Router, Route, Outlet } from "svelte-guard-history-router";
+  import { base } from "./constants.mjs";
+  import Home from "./Home.svelte";
 </script>
 
-<Router>
-  <PullRequestCard {pullRequest} />
-  <RepositoryGroupCard {repositoryGroup} />
-  <HookCard {hook} />
+<Router {base}>
+  <nav>
+    <Route href="/" path="*" component={Home}>Repository Example</Route>
+  </nav>
+
+  <main>
+    <Outlet />
+  </main>
 </Router>
-
-<AttributeField attribute={{ name: 'a1', value: 'value a1' }} />
-<SecureAttributeField attribute={{ name: 's1', value: 'value s1' }} />
-
-<ContentEntryCard {entry} />
---
+++ tests/app/src/Home.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { MultiGroupProvider, PullRequest, Hook } from "repository-provider";
+  import { StringContentEntry } from "content-entry";
+  import {
+    AttributeField,
+    SecureAttributeField,
+    PullRequestCard,
+    RepositoryGroupCard,
+    HookCard,
+    ContentEntryCard
+  } from "../../../src/index.svelte";
+
+  const provider = new MultiGroupProvider();
+  const repositoryGroup = provider.addRepositoryGroup("rg1");
+  const repository = repositoryGroup.addRepository("r1");
+  const b1 = repository.addBranch("b1");
+  const b2 = repository.addBranch("b2");
+  const pullRequest = new PullRequest(b1, b2, 4711);
+
+  const hook = new Hook(repository, "h1");
+
+  const entry = new StringContentEntry("entry name", "lines 1");
+</script>
+
+<div class="canvas">
+  <PullRequestCard {pullRequest} />
+  <RepositoryGroupCard {repositoryGroup} />
+  <HookCard {hook} />
+
+  <AttributeField attribute={{ name: "a1", value: "value a1" }} />
+  <SecureAttributeField attribute={{ name: "s1", value: "value s1" }} />
+
+  <ContentEntryCard {entry} />
+</div>
